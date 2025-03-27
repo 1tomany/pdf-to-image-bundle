803ee9b52a5f9285580f627d4d5544ec2c6a274f
Vic Cherubini <vcherubini@gmail.com>
2025-03-27 16:27:42 -0500

    initial commit

---
+++ src/PdfToImageBundle.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace OneToMany\PdfToImageBundle;
+
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
+use Symfony\Component\HttpKernel\Bundle\AbstractBundle;
+
+class PdfToImageBundle extends AbstractBundle
+{
+
+    /**
+     * @param array<string, mixed> $config
+     */
+    public function loadExtension(array $config, ContainerConfigurator $container, ContainerBuilder $builder): void
+    {
+        $container->import('../config/services.xml');
+    }
+
+}
